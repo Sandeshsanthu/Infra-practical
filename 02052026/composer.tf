ad39fe51c0e5278780043ede0cf4d162520a28b0
Sandeshsanthu <sandesh.suresh18@gmail.com>
2026-02-05 15:52:48 +0000

    composer

--- 02052026/composer.tf
+++ 02052026/composer.tf
@@ -1,5 +1,18 @@
+locals {
+composer_envs = {
+dev  = "example-composer-env-tf-c3-dev"
+prod = "example-composer-env-tf-c3-test"
+}
+}
+resource "google_project_iam_member" "composer-worker" {
+  project = "project-d1a95002-78b0-493b-b7e"
+  role    = "roles/composer.worker"
+  member  = "serviceAccount:composer-env-account@project-d1a95002-78b0-493b-b7e.iam.gserviceaccount.com"
+}
+
 resource "google_composer_environment" "test" {
-  name   = "example-composer-env-tf-c3"
+  for_each = local.composer_envs
+  name   = each.value
   region = "us-central1"
   config {
 
@@ -48,8 +61,3 @@ resource "google_composer_environment" "test" {
 }
 
 
-resource "google_project_iam_member" "composer-worker" {
-  project = "project-d1a95002-78b0-493b-b7e"
-  role    = "roles/composer.worker"
-  member  = "serviceAccount:composer-env-account@project-d1a95002-78b0-493b-b7e.iam.gserviceaccount.com"
-}
